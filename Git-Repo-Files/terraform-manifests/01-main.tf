5513f7d6d7a577f4d7e18cde7a03700f87f45026
pavan1012 <48289006+pavan1012@users.noreply.github.com>
2022-11-24 02:09:26 +0530

    Update 01-main.tf

--- Git-Repo-Files/terraform-manifests/01-main.tf
+++ Git-Repo-Files/terraform-manifests/01-main.tf
@@ -14,15 +14,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.32.0"
+      version = "~> 2.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.30.0"
+      version = "~> 2.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.4.3"
+      version = "~> 3.0"
     }
   }
 /*
