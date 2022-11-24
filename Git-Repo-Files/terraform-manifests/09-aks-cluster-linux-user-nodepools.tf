fa5b02bf7fe6610917ce9e378371e8a4bbc25e83
pavan1012 <48289006+pavan1012@users.noreply.github.com>
2022-11-24 23:39:41 +0530

    Update 09-aks-cluster-linux-user-nodepools.tf

--- Git-Repo-Files/terraform-manifests/09-aks-cluster-linux-user-nodepools.tf
+++ Git-Repo-Files/terraform-manifests/09-aks-cluster-linux-user-nodepools.tf
@@ -4,7 +4,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "linux101" {
   availability_zones    = [1, 2, 3]
   enable_auto_scaling   = true
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
-  max_count             = 3
+  max_count             = 1
   min_count             = 1
   mode                  = "User"
   name                  = "linux101"
