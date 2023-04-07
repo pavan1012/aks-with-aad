477327607abdbb754b5c30aa927cc0d7e107cc7d
pavan1012 <48289006+pavan1012@users.noreply.github.com>
2023-04-07 11:58:12 +0530

    Update 02-variables.tf

--- Git-Repo-Files/terraform-manifests/02-variables.tf
+++ Git-Repo-Files/terraform-manifests/02-variables.tf
@@ -21,7 +21,7 @@ variable "resource_group_name" {
 variable "environment" {
   type = string  
   description = "This variable defines the Environment"  
-  #default = "dev2"
+  default = "dev"
 }
 
 
@@ -29,7 +29,7 @@ variable "environment" {
 
 # SSH Public Key for Linux VMs
 variable "ssh_public_key" {
-  #default = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
+  default = "~/.ssh/aks-prod-sshkeys-terraform/aksprodsshkey.pub"
   description = "This variable defines the SSH Public Key for Linux k8s Worker nodes"  
 }
 
